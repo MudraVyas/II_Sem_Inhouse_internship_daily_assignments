a2a27b81c4dd00f9d97ad9dbce9686671bd601a5
MudraVyas <mudravyas28@gmail.com>
2026-07-13 19:08:25 +0530

    Add files via upload

--- 25ESKCS232/day12/db.php
+++ 25ESKCS232/day12/db.php
@@ -1,1 +1,23 @@
+<?php
+$host = "localhost";
+$dbname = "student_management";
+$username = "root";
+$password = "";
 
+try {
+    $pdo = new PDO(
+        "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+        $username,
+        $password
+    );
+
+    // Enable error reporting
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Fetch data as associative array
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+} catch (PDOException $e) {
+    die("Connection Failed: " . $e->getMessage());
+}
+?>
