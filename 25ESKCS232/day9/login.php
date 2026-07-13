e233368665aa7a59f84ba35268d91ac191d65af4
MudraVyas <mudravyas28@gmail.com>
2026-07-13 19:04:19 +0530

    Add files via upload

--- 25ESKCS232/day9/login.php
+++ 25ESKCS232/day9/login.php
@@ -1,21 +1,24 @@
 <?php
 include ("header.php");
 include ("db_connect.php");
-include ("checkRegistrationError.php");
+include ("checkLoginError.php");
 ?>
 
 <div class="container mt-5" style="max-width:400px;">
     <form action="" method = "post">
-        <h3 class="mb-3">Register</h3>
-
-        <input type="text" name="name" class="form-control mb-3" placeholder="Name" value="<?= $name?>">
+        <h3 class="mb-3">Login</h3>
 
         <input type="email" class="form-control mb-3" name="email" placeholder="Email" value="<?= $email?>">
 
         <input type="password" class="form-control mb-3" placeholder="Password" name="password" value="<?= $password?>">
 
-        <input type="password" class="form-control mb-3" placeholder="Confirm Password" name="confirmPassword" value="<?= $confirmPassword?>">
 
-        <button class="btn btn-primary w-100">Register</button>
+        <button class="btn btn-primary w-100">Login</button>
     </form>
 </div>
+
+
+
+
+<?php 
+include ("footer.php"); ?>
